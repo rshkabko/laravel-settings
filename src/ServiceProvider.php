23519c33fbba40df9fc5eadbf3e393f3582109c0
rshkabko <r.shkabko@flamix.email>
2023-06-02 18:08:23 +0300

    Small fix

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -28,7 +28,7 @@ class ServiceProvider extends BaseServiceProvider
         });
 
         $this->app->alias('Flamix\Settings\SettingStore', 'setting');
-        $this->mergeConfigFrom(__DIR__ . '/config/config.php', 'settings');
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'settings');
     }
 
     /**
@@ -37,10 +37,10 @@ class ServiceProvider extends BaseServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/config/config.php' => config_path('settings.php')
+            __DIR__ . '/../config/config.php' => config_path('settings.php')
         ], 'config');
         $this->publishes([
-            __DIR__ . '/migrations/2015_08_25_172600_create_settings_table.php' => database_path('migrations/' . date('Y_m_d_His') . '_create_settings_table.php')
+            __DIR__ . '/../database/migrations/2015_08_25_172600_create_settings_table.php' => database_path('migrations/' . date('Y_m_d_His') . '_create_settings_table.php')
         ], 'migrations');
     }
 
