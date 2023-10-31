35ae661d9c164ff681275c3aa4e6e44d1436a6c1
Roman Shkabko <r.shkabko@flamix.email>
2023-10-31 11:59:08 +0200

    Update ServiceProvider.php - Migration automatic

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -39,9 +39,12 @@ class ServiceProvider extends BaseServiceProvider
         $this->publishes([
             __DIR__ . '/../config/config.php' => config_path('settings.php')
         ], 'config');
+        
         $this->publishes([
-            __DIR__ . '/../database/migrations/2015_08_25_172600_create_settings_table.php' => database_path('migrations/' . date('Y_m_d_His') . '_create_settings_table.php')
+            __DIR__ . '/../database/migrations/' => database_path('migrations'),
         ], 'migrations');
+
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
 
     /**
